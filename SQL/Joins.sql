62d3c4a79496bcfbcb9c3e7b0a7327b1d3872f7c
Ruturaj Narhe <165364911+ruturajnarhe@users.noreply.github.com>
2024-07-31 23:11:51 +0530

    Add files via upload

---
+++ SQL/Joins.sql
@@ -0,0 +1,39 @@
+create database joins;
+
+use joins;
+
+create table student(
+	id int primary key,
+    name varchar(50)
+);
+
+insert into student (id, name) values
+(101, "Ravi"),
+(102, "Raj"),
+(103, "Harsh");
+
+create table cource(
+	id int,
+    cource varchar(50)
+);
+
+insert into cource (id, cource) values
+(102, "english"),
+(105, "math"),
+(103, "science"),
+(107, "CS");
+
+select * from student
+inner join cource
+on student.id = cource.id;
+
+select *from student
+left join cource
+on student.id = cource.id;
+
+select *from student
+right join cource
+on student.id = cource.id;
+
+
+
